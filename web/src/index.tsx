662e05062d0cd8b31f02313caccdd1403d9e5ff1
Chris Penner <christopher.penner@gmail.com>
2020-06-05 00:18:45 -0600

    Clean up styling

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,81 +1,174 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import FileCopy from '@material-ui/icons/FileCopyOutlined';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import {
+  Grid,
+  Button,
+  Typography,
+  Card,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+} from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
 
 const SimpleInput: React.SFC<{
   name: string;
   id: string;
   setter: (s: string) => void;
-}> = ({ name, id, setter }) => (
-  <label>
-    {name}
-    <input
-      type="text"
-      name={id}
-      id={id}
-      placeholder={name}
-      onChange={(e) => setter(e.target.value)}
-    />
-  </label>
+  required?: boolean;
+  multiline?: boolean;
+  error?: boolean;
+  helperText?: string;
+  placeholder?: string;
+  value: string;
+}> = ({ name, id, setter, required, multiline, ...rest }) => (
+  <TextField
+    className="field"
+    type="text"
+    name={id}
+    label={name}
+    id={id}
+    onChange={(e) => setter(e.target.value)}
+    variant="outlined"
+    required={required}
+    multiline={multiline}
+    rows={multiline ? 10 : 0}
+    {...rest}
+  />
 );
 
 interface LinkParts {
   recipient: string;
   subject: string;
   body: string;
-  /* bcc: string; */
+  bcc?: string[];
 }
 
 const buildLink: (parts: LinkParts) => string = ({
   recipient,
   body,
   subject,
-  /* bcc, */
+  bcc = [],
 }) =>
-  `mailto:${recipient}?subject=${encodeURIComponent(
+  `mailto:${encodeURIComponent(recipient || '')}?subject=${encodeURIComponent(
     subject || ''
-  )}&body=${encodeURIComponent(body || '')}`;
-
-const EmailLink: React.SFC<LinkParts> = (linkParts) => {
-  const link = buildLink(linkParts);
-  return (
-    <div>
-      <a href={link}> Test </a>
-      <CopyToClipboard
-        text={link}
-        onCopy={() => alert('Copied link to your clipboard')}
-      >
-        <button>Copy</button>
-      </CopyToClipboard>
-    </div>
-  );
-};
+  )}&body=${encodeURIComponent(body || '')}&bcc=${encodeURIComponent(
+    bcc.join('; ')
+  )}`;
 
 const Application: React.SFC<{}> = () => {
   const [recipient, setRecipient] = useState('');
   const [subject, setSubject] = useState('');
   const [body, setBody] = useState('');
-  /* const [bcc, setBCC] = useState(''); */
+  const [bcc, setBCC] = useState([] as string[]);
+  const link = buildLink({ recipient, subject, body, bcc });
   return (
-    <div>
-      <h1>Digital Megaphone</h1>
-      <form>
-        <SimpleInput
-          name="Primary Recipient"
-          id="recipient"
-          setter={setRecipient}
-        />
-        <SimpleInput name="Subject" id="subject" setter={setSubject} />
-        {/* <SimpleInput name="BCC" id="bcc" setter={setBCC} /> */}
-        <SimpleInput name="Body" id="body" setter={setBody} />
-        <EmailLink
-          recipient={recipient}
-          subject={subject}
-          body={body}
-          // bcc={bcc}
-        />
-      </form>
-    </div>
+    <Card className="card">
+      <Grid
+        className="main-grid"
+        container
+        spacing={2}
+        alignItems="center"
+        direction="column"
+      >
+        <Grid item xs={12}>
+          <Typography variant="h2">Digital Megaphone</Typography>
+        </Grid>
+        <Grid item xs={12} className="full-width">
+          <ExpansionPanel>
+            <ExpansionPanelSummary color="primary" expandIcon={<ExpandMore />}>
+              <Typography> What's this for? </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                This is a tool for helping to construct shareable links, which
+                when clicked open an e-mail app with the full email pre-filled
+                with a useful starting point.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </Grid>
+        <Grid item xs={12} className="full-width">
+          <SimpleInput
+            name="Primary Recipient"
+            placeholder="recipient@example.com"
+            id="recipient"
+            setter={setRecipient}
+            required
+            helperText={subject ? '' : 'A recipient is required'}
+            error={!recipient}
+            value={recipient}
+          />
+        </Grid>
+        <Grid item className="full-width">
+          <ChipInput
+            newChipKeys={['Enter', ',', ';', ' ']}
+            onChange={(chips) => setBCC(chips)}
+            className="field"
+            id="bcc"
+            label="BCC"
+            variant="outlined"
+            placeholder="secondary-recipient@example.com"
+          ></ChipInput>
+        </Grid>
+        <Grid item className="full-width">
+          <SimpleInput
+            name="Subject"
+            id="subject"
+            setter={setSubject}
+            error={!subject}
+            helperText={subject ? '' : 'A subject is required'}
+            required
+            value={subject}
+            placeholder="What's your email about?"
+          />
+        </Grid>
+        <Grid item className="full-width">
+          <SimpleInput
+            value={body}
+            name="Body"
+            id="body"
+            setter={setBody}
+            placeholder="Type your message here..."
+            multiline
+          />
+        </Grid>
+        <div className="actions">
+          <div className="action-row">
+            <a href={link}>
+              <Button className="test" variant="contained" color="primary">
+                Test Link{' '}
+              </Button>
+            </a>
+            <CopyToClipboard
+              text={link}
+              onCopy={() => alert('Copied link to your clipboard')}
+            >
+              <Button
+                className="copy-to-clipboard"
+                variant="outlined"
+                color="secondary"
+              >
+                <Grid container alignItems="center" spacing={2}>
+                  <Grid item>Copy to Clipboard</Grid>
+                  <Grid item>
+                    <FileCopy />
+                  </Grid>
+                </Grid>
+              </Button>
+            </CopyToClipboard>
+          </div>
+          <div className="action-row">
+            <code>{link}</code>
+          </div>
+        </div>
+      </Grid>
+    </Card>
   );
 };
 
