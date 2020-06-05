9ce23928d531a2e509f8aa181d7ad26337e7dcd2
Chris Penner <christopher.penner@gmail.com>
2020-06-05 09:43:07 -0600

    Fix code growing off screen

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -71,87 +71,67 @@ const Application: React.SFC<{}> = () => {
   const [cc, setCC] = useState([] as string[]);
   const link = buildLink({ recipient, subject, body, bcc, cc });
   return (
-    <Card className="card">
-      <Grid
-        className="main-grid"
-        container
-        spacing={2}
-        alignItems="center"
-        direction="column"
-      >
-        <Grid item xs={12}>
-          <Typography variant="h2">Digital Megaphone</Typography>
-        </Grid>
-        <Grid item xs={12} className="full-width">
-          <ExpansionPanel>
-            <ExpansionPanelSummary color="primary" expandIcon={<ExpandMore />}>
-              <Typography> What's this for? </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                This is a tool for helping to construct shareable links, which
-                when clicked open an e-mail app with the full email pre-filled
-                with a useful starting point.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </Grid>
-        <Grid item xs={12} className="full-width">
-          <SimpleInput
-            name="Primary Recipient"
-            placeholder="recipient@example.com"
-            id="recipient"
-            setter={setRecipient}
-            required
-            helperText={subject ? '' : 'A recipient is required'}
-            error={!recipient}
-            value={recipient}
-          />
-        </Grid>
-        <Grid item className="full-width">
-          <ChipInput
-            newChipKeys={['Enter', ',', ';', ' ']}
-            onChange={(chips) => setCC(chips)}
-            className="field"
-            id="cc"
-            label="CC"
-            variant="outlined"
-            placeholder="cc-recipient@example.com"
-          ></ChipInput>
-        </Grid>
-        <Grid item className="full-width">
-          <ChipInput
-            newChipKeys={['Enter', ',', ';', ' ']}
-            onChange={(chips) => setBCC(chips)}
-            className="field"
-            id="bcc"
-            label="BCC"
-            variant="outlined"
-            placeholder="bcc-recipient@example.com"
-          ></ChipInput>
-        </Grid>
-        <Grid item className="full-width">
-          <SimpleInput
-            name="Subject"
-            id="subject"
-            setter={setSubject}
-            error={!subject}
-            helperText={subject ? '' : 'A subject is required'}
-            required
-            value={subject}
-            placeholder="What's your email about?"
-          />
-        </Grid>
-        <Grid item className="full-width">
-          <SimpleInput
-            value={body}
-            name="Body"
-            id="body"
-            setter={setBody}
-            placeholder="Type your message here..."
-            multiline
-          />
-        </Grid>
+    <div class="main">
+      <Card className="card">
+        <Typography variant="h2">Digital Megaphone</Typography>
+        <ExpansionPanel>
+          <ExpansionPanelSummary color="primary" expandIcon={<ExpandMore />}>
+            <Typography> What's this for? </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              This is a tool for helping to construct shareable links, which
+              when clicked open an e-mail app with the full email pre-filled
+              with a useful starting point.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <SimpleInput
+          name="Primary Recipient"
+          placeholder="recipient@example.com"
+          id="recipient"
+          setter={setRecipient}
+          required
+          helperText={subject ? '' : 'A recipient is required'}
+          error={!recipient}
+          value={recipient}
+        />
+        <ChipInput
+          newChipKeys={['Enter', ',', ';', ' ']}
+          onChange={(chips) => setCC(chips)}
+          className="field"
+          id="cc"
+          label="CC"
+          variant="outlined"
+          placeholder="cc-recipient@example.com"
+        ></ChipInput>
+        <ChipInput
+          newChipKeys={['Enter', ',', ';', ' ']}
+          onChange={(chips) => setBCC(chips)}
+          className="field"
+          id="bcc"
+          label="BCC"
+          variant="outlined"
+          placeholder="bcc-recipient@example.com"
+        ></ChipInput>
+        <SimpleInput
+          name="Subject"
+          id="subject"
+          setter={setSubject}
+          error={!subject}
+          helperText={subject ? '' : 'A subject is required'}
+          required
+          value={subject}
+          placeholder="What's your email about?"
+        />
+        <SimpleInput
+          value={body}
+          name="Body"
+          id="body"
+          setter={setBody}
+          placeholder="Type your message here..."
+          multiline
+        />
         <div className="actions">
           <div className="action-row">
             <a href={link}>
@@ -181,8 +161,8 @@ const Application: React.SFC<{}> = () => {
             <code>{link}</code>
           </div>
         </div>
-      </Grid>
-    </Card>
+      </Card>
+    </div>
   );
 };
 
